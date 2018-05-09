c3d93871b6be9166bfc281660cccba5fef2f81b2
Quirico Tursi <quirico.tursi@gmail.com>
2018-05-09 17:47:43 +0200

    Generating a race condition with two served files.

--- server.js
+++ server.js
@@ -13,10 +13,15 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     console.log(`serving request #${req.requestNumber}`);
 
-    // Serve a file for first request and nothing for the rest
-    if (req.requestNumber == 1) {
-        fs.readFile("file.txt", "utf8", (err, data) => {
-            console.log(`file read for request #1 complete`);
+    /*
+    /   Serve a file for request #1 and #82 and nothing for the rest.
+    /   file1.txt is 'heavier' than file82.txt so the former should take more
+    /   time to be fulfilled (its request will likely be the latest one
+    /   to be logged).
+    */
+    if (req.requestNumber == 1 || req.requestNumber == 82) {
+        fs.readFile(`file${req.requestNumber}.txt`, "utf8", (err, data) => {
+            console.log(`file read for request #${req.requestNumber} complete`);
             res.send(data);
         });
     } else {
@@ -29,4 +34,4 @@ var server = app.listen(3000);
 // Close server and exit Node.js after a while
 setTimeout(() => {
     server.close();
-}, 5 * 1000);
+}, 10 * 1000);
